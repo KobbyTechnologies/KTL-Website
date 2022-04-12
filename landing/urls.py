c8a23974ddd1c942d956c3c6fa7711a864e71e0e
Motanya Nyabanga <francismotanya@outlook.com>
2022-04-12 17:43:58 +0300

    solution pages update

--- landing/urls.py
+++ landing/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='index'),
-
+    path('solution/', views.solution, name='solution'),
+    path('solution-single/', views.solution_view, name='solution_single'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def landing(request):
     return render(request, 'index.html')
+    
+def solution(request):
+    return render(request, 'solution.html')
+
+
+def solution_view(request):
+    return render(request, 'single_solution.html')
+
+
