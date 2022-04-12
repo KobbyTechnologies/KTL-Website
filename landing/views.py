26f8e56d1c1995f6b533e9c1996089bfcf06bbd1
enockabere <maebaenock95@gmail.com>
2022-04-12 21:31:15 +0300

    add css

--- landing/views.py
+++ landing/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 
 
 def landing(request):
+    
+    
+    
     return render(request, 'index.html')
+
+def about_us(request):
+    today = "Tuesday"
+    ctx= {"leo":today}
+    return render(request,"about.html")
