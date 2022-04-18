7128941d989a90c2b15a5080dc83e1771267bf8e
Motanya Nyabanga <francismotanya@outlook.com>
2022-04-18 19:00:09 +0300

    Library layout

--- library/urls.py
+++ library/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('library/', views.stories_view, name='library'),
+    path('success_stories/', views.stories_view, name='success-stories'),
+    path('story/', views.story_detail, name='read_more'),
+    path('events/', views.events, name='events'),
+    path('csr/', views.csr_view, name='csr'),
+    path('csr-detail/', views.csr_detail_view, name='csr-detail'),
 ]
--- library/views.py
+++ library/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def  stories_view(request):
     return render(request, 'success-stories.html')
+
+
+def  story_detail(request):
+    return render(request, 'story.html')
+
+
+def events(request):
+    return render(request, 'events.html')
+
+
+def csr_view(request):
+    return render(request, 'csr.html')
+    
+    
+def csr_detail_view(request):
+    return render(request, 'csr-detail.html')
