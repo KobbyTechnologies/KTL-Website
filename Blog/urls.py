fad71a79ef69f1712f6b208f06a3c121cea1abf2
beryl2 <berylakinyi150@gmail.com>
2022-04-28 11:01:05 +0300

    blog

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path('Blog',views.Blog_request, name='Blog'),
+    path('BLOG2',views.BLOG2_request, name='BLOG2'),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def Blog_request(request):
     return render(request, 'Blog.html')
+
+def BLOG2_request(request):
+    return render(request, 'BLOG2.html')    
