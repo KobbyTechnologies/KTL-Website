0a6d69f082637c55223103aab670fe795f8c62d7
enockabere <maebaenock95@gmail.com>
2022-04-28 09:43:18 +0300

    changes

--- landing/urls.py
+++ landing/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='index'),
-    path('about', views.about_us, name='about'),
 ]
 
--- landing/views.py
+++ landing/views.py
@@ -4,12 +4,6 @@ from django.shortcuts import render
 
 
 def landing(request):
-    
-    
-    
+   
     return render(request, 'index.html')
 
-def about_us(request):
-    today = "Tuesday"
-    ctx= {"leo":today}
-    return render(request,"about.html")
