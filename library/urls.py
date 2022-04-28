8134c54ad28ef3a0882d0ae8dbe4ae5737db0505
enockabere <maebaenock95@gmail.com>
2022-04-28 15:42:26 +0300

    deploy

--- library/urls.py
+++ library/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('success_stories/', views.stories_view, name='success-stories'),
-    path('story/', views.story_detail, name='read_more'),
-    path('events/', views.events, name='events'),
-    path('csr/', views.csr_view, name='csr'),
-    path('csr-detail/', views.csr_detail_view, name='csr-detail'),
+    path('success_stories', views.stories_view, name='success-stories'),
+    path('story', views.story_detail, name='read_more'),
+    path('events', views.events, name='events'),
+    path('csr', views.csr_view, name='csr'),
+    path('csr-detail', views.csr_detail_view, name='csr-detail'),
     path('gallery/', views.gallery_view, name='gallery'),
 ]
