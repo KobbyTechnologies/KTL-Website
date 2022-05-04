ae6978221635af0598cd23a5399058d6c36fd56e
beryl2 <berylakinyi150@gmail.com>
2022-05-04 17:02:17 +0300

    merge

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('Blog',views.Blog_request, name='Blog'),
-    path('Blogdetails',views.BLOG2_request, name='Blogdetails'),
+    path('Blogdetails',views.Blog_request, name='Blogdetails'),
+    path('Blog',views.BLOG2_request, name='Blog'),
 ]
