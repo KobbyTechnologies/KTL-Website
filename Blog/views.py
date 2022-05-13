4343bd44dad8119e68f905c0aa1cd2be438f00dc
enockabere <maebaenock95@gmail.com>
2022-05-13 16:01:03 +0300

    deploy

--- Blog/views.py
+++ Blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def blog_request(request):
-    return render(request, 'blog.html')
+    return render(request, 'Blog.html')
 
 def blog_detail_request(request):
     return render(request, 'blog-details.html')    
